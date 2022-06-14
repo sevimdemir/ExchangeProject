d2ae8a02ea24f35b6b2f0ee8f76b328fe6112721
Sevim Demir <sevimmdemir@gmail.com>
2022-06-15 00:37:11 +0300

    unit test updated

--- src/test/java/com/openpayd/exchange/service/impl/ExchangeRateServiceTest.java
+++ src/test/java/com/openpayd/exchange/service/impl/ExchangeRateServiceTest.java
@@ -2,6 +2,7 @@ package com.openpayd.exchange.service.impl;
 
 import com.github.javafaker.Faker;
 import com.openpayd.exchange.dto.ExchangeRate;
+import com.openpayd.exchange.dto.FixerApiResponse;
 import com.openpayd.exchange.exception.BaseException;
 import com.openpayd.exchange.exception.NoCurrencyFoundException;
 import com.openpayd.exchange.persist.repository.IExchangeRatesRepository;
@@ -12,12 +13,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,6 +54,22 @@ class ExchangeRateServiceTest {
             assertEquals(0, BigDecimal.ONE.compareTo(todaysRateOf.getRate()));
         } catch (BaseException e) {
             fail("Unexpected BaseException ", e);
+        }
+    }
+
+    @Test
+    void getTodaysRateOfWithException() {
+        try {
+            ResponseEntity<FixerApiResponse> httpResponse = Mockito.mock(ResponseEntity.class);
+            when(httpResponse.getBody()).thenReturn(new FixerApiResponse());
+            when(exchangeRatesRepository.getTodaysRateOf(Mockito.anyString())).thenThrow(new NoCurrencyFoundException(Faker.instance().currency().code()));
+            when(environment.getProperty("api.exchange.key")).thenReturn(Faker.instance().lorem().word());
+            when(environment.getProperty("api.exchange.uri")).thenReturn("https://api.apilayer.com/fixer/latest");
+            when(restTemplate.exchange(Mockito.anyString(), Mockito.any(HttpMethod.class), Mockito.any(HttpEntity.class), Mockito.any(Class.class), Mockito.any(Map.class))).thenReturn(httpResponse);
+            ExchangeRate todaysRateOf = exchangeRateService.getTodaysRateOf(Faker.instance().currency().code());
+            verify(restTemplate).exchange(Mockito.anyString(), Mockito.any(HttpMethod.class), Mockito.any(HttpEntity.class), Mockito.any(Class.class), Mockito.any(Map.class));
+        } catch (BaseException e) {
+            assertEquals(e.getClass(), NoCurrencyFoundException.class);
         }
     }
 }
